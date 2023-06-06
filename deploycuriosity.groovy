1b203a49b3ae51bda09772de3c11994e9f60cb8e
fharris <fharris.dev@gmail.com>
2023-06-06 23:47:28 +0200

    Update deploycuriosity.groovy

--- deploycuriosity.groovy
+++ deploycuriosity.groovy
@@ -1,1 +1,61 @@
+#!/usr/bin/env groovy
+
+pipeline {
+  environment {
+    registry = "fharris/curiosity"
+    registryCredential = 'id-docker-registry'
+    dockerImageBuild = ''
+    dockerImageLatest = ''
+    kubernetes_proxy = 'https://192.168.5.15:6443'
+  }
+  agent any
+  stages{
+      
+    stage('Get source code') {
+      steps {
+        git branch: 'main', 
+            url: 'http://gogs:3000/gogs-user/curiositymonolith.git'
+      }
+    }
+
+    stage('Checkout code') {
+        steps {
+            checkout scm
+        }
+    }
+      stage('Checking source code') {
+      steps {
+        sh 'ls -ltra'
+      }
+    }
+
+
+      
+      stage('Deployment  to Kubernetes') {
+      steps {
+        withKubeConfig( credentialsId: 'jenkins-token-kubernetes', serverUrl: kubernetes_proxy ) {
+            sh "kubectl apply -f curiositymonolith-configmap.yaml"
+            sh "kubectl describe configmap curiositymonolith-configmap -n curiositymonolith"
+            sh "kubectl apply -f  curiositymonolith-deployment.yaml -n curiositymonolith"
+            sh "kubectl rollout restart -f curiositymonolith-deployment.yaml"
+            sh "kubectl get deployments -n curiositymonolith"
+            sh "kubectl get pods -n curiositymonolith"
+        }
+      }
+    }
+
+  
+  }
+  post {
+        // Clean after build
+        always {
+            cleanWs(cleanWhenNotBuilt: false,
+                    deleteDirs: true,
+                    disableDeferredWipeout: true,
+                    notFailBuild: true,
+                    patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
+                               [pattern: '.propsfile', type: 'EXCLUDE']])
+        }
+    }
+}
 
