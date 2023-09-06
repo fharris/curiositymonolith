4ae52d826df8cb949b39013833cf730fc1f36dcc
fharris <fharris.dev@gmail.com>
2023-09-06 15:52:43 +0200

    Create create-curiositydb-resources.sql

---
+++ databaseconfig/create-curiositydb-resources.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS curiositydb;
+DROP USER IF EXISTS curiosity;
+CREATE DATABASE curiositydb;
+CREATE USER 'curiosity'@'%' IDENTIFIED BY 'Welcome#1';
+GRANT ALL PRIVILEGES ON curiositydb.* TO 'curiosity'@'%'; 
