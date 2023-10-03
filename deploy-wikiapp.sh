0e73e7e22e340fce3bc8a9f9c39bb3c142122c82
fernando <fernando.harris@oracle.com>
2023-10-03 13:07:41 +0200

    updating for a public accessible image of the application

--- deploy-wikiapp.sh
+++ deploy-wikiapp.sh
@@ -1,3 +1,5 @@
+
+echo "Deploying Curiosity Monolith"
 kubectl apply -f appconfig/curiositymonolith-namespace.yaml;
 kubectl apply -f ./databaseconfig/.
 sleep 15;
@@ -5,7 +7,7 @@ kubectl -n curiositymonolith exec -it  `kubectl -n curiositymonolith get \
  --no-headers=true pods -l app=mysql-db -o custom-columns=:metadata.name` \
  -- mysql -h 127.0.0.1 -u root -pmySQLpword#2023 < ./databaseconfig/create-curiositydb-resources.sql;
 kubectl apply -f ./appconfig/. ;
-
+echo "updating for a public accessible image of the application"
+kubectl -n curiositymonolith set image deployment/curiositymonolith-deployment curiosity=fharris/curiosity:latest
+sleep 10;
 kubectl get pods -n curiositymonolith;
-
-
