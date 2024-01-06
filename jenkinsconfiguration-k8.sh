0fd1965a6ea89a2cd3b4d8f7af9b89a251f7c0f3
fharris <fharris.dev@gmail.com>
2024-01-06 18:51:14 +0100

    Update jenkinsconfiguration-k8.sh

--- jenkinsconfiguration-k8.sh
+++ jenkinsconfiguration-k8.sh
@@ -9,7 +9,7 @@ echo Running in $SHELL
 kubectl apply -f appconfig/curiositymonolith-namespace.yaml
 kubectl apply -f jenkins-k8s-config/jenkins-task-sa.yaml
 kubectl apply -f jenkins-k8s-config/jenkins-task-sa-secret.yaml
-export JENKINS_TOKEN=`kubectl get secrets jenkins-task-sa-secret -o json | jq -Mr '.data["token"]' | base64 -D;`
+export JENKINS_TOKEN=`kubectl get secrets jenkins-task-sa-secret -o json | jq -Mr '.data["token"]' | base64 -d;`
 echo "Save below token to past later in Jenkins"
 echo $JENKINS_TOKEN
 kubectl apply -f jenkins-k8s-config/jenkins-clusterrole.yaml
