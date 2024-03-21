67080e69a334d8578d239c66ff8dc8ec372a61b4
Fernando Harris <fernando.harris@oracle.com>
2024-03-21 12:53:50 +0100

    changed MySQL image to support ARM

--- deploy-curiosity.sh
+++ deploy-curiosity.sh
@@ -1,13 +1,30 @@
 
+
+echo "############################"
+echo "############################"
+echo "############################"
+echo "############################"
 echo "Deploying Curiosity Monolith"
+echo "############################"
+echo "############################"
+echo "############################"
+echo "############################"
 kubectl apply -f appconfig/curiositymonolith-namespace.yaml;
 kubectl apply -f ./databaseconfig/.
-sleep 15;
+echo ".... waiting for MySQL pod to run to run db configuration...Deploying Curiosity Monolith"
+echo "############################"
+echo "############################"
+sleep 45;
 kubectl -n curiositymonolith exec -it  `kubectl -n curiositymonolith get \
  --no-headers=true pods -l app=mysql-db -o custom-columns=:metadata.name` \
  -- mysql -h 127.0.0.1 -u root -pmySQLpword#2023 < ./databaseconfig/create-curiositydb-resources.sql;
 kubectl apply -f ./appconfig/. ;
 echo "updating for a public accessible image of the application"
 kubectl -n curiositymonolith set image deployment/curiositymonolith-deployment curiosity=fharris/curiosity:latest
-sleep 10;
+echo ".... waiting for the application to get deployed..."
+sleep 20;
 kubectl get pods -n curiositymonolith;
+echo "############################"
+echo "Done... Have fun!"
+echo "############################"
+
